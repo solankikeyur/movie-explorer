7190bfc22f17a809e16ba7872ee3a474f647b3ee
keyursolanki7071 <keyur.solanki@arsenaltech.com>
2025-04-15 00:14:38 +0530

    Installed tailwind , pinia and vue router

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import AppLayout from './components/Layout/AppLayout.vue';
+</script>
+
+<template>
+  <AppLayout>
+    <RouterView/>
+  </AppLayout>
+</template>
+
+<style scoped>
+
+</style>
